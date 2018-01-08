e555c4c22b2b3e751dbcfeed9c4da46e14e554e2
Sirius Dely <siriusdely@icloud.com>
2018-01-08 21:30:27 +0700

    rubocop & coveralls

--- lib/BCA.rb
+++ lib/BCA.rb
@@ -1,15 +1,12 @@
-require "BCA/version"
-require "curb"
-require "nokogiri"
+require 'BCA/version'
+require 'curb'
+require 'nokogiri'
 
 module BCA
-
-  USER_AGENT = "BCA-#{VERSION}.gem"
-  # BASE_URL = "https://www.pancasila.co".freeze
-  BASE_URL = "https://ibank.klikbca.com".freeze
+  USER_AGENT = "BCA-#{VERSION}.gem".freeze
+  BASE_URL = 'https://ibank.klikbca.com'.freeze
 
   class Client
-
     def initialize(username, password)
       @username = username
       @password = password
@@ -17,91 +14,91 @@ module BCA
 
     def login
       curl = Curl::Easy.new("#{BASE_URL}/authentication.do")
-      curl.headers["User-Agent"] = USER_AGENT
+      curl.headers['User-Agent'] = USER_AGENT
       # curl.verbose = true
 
       @cookies = []
-      curl.on_header { |header|
-        # p header
-        @cookies << "#{$1}=#{$2}" if header =~ /^Set-Cookie: ([^=]+)=([^;]+)/
+      curl.on_header do |header|
+        @cookies << "#{Regexp.last_match(1)}=#{Regexp.last_match(2)}" if
+        header =~ /^Set-Cookie: ([^=]+)=([^;]+)/
         header.length
-      }
+      end
 
       curl.http_post(
-        Curl::PostField.content("value(actions)", "login"),
-        Curl::PostField.content("value(user_id)", @username),
-        Curl::PostField.content("value(pswd)", @password),
+        Curl::PostField.content('value(actions)', 'login'),
+        Curl::PostField.content('value(user_id)', @username),
+        Curl::PostField.content('value(pswd)', @password),
         # Curl::PostField.content("value(user_ip)", "111.94.26.135"),
         # Curl::PostField.content("value(mobile)", "false"),
-        Curl::PostField.content("value(Submit)", "LOGIN")
+        Curl::PostField.content('value(Submit)', 'LOGIN')
       )
 
+      # rubocop:disable Style/IfInsideElse
       if curl.response_code != 200
-        message = "#{$1}" if curl.body_str =~ /<title>(.+?)<\/title>/
+        message = Regexp.last_match(1).to_s if
+          curl.body_str =~ %r{<title>(.+?)<\/title>}
       else
-        message = "#{$1}" if curl.body_str =~  /var err=\s*'([^';]*)/
+        message = Regexp.last_match(1).to_s if
+          curl.body_str =~ /var err=\s*'([^';]*)/
       end
+      # rubocop:enable Style/IfInsideElse
 
       if message
         @cookies = nil
         [false, message]
       else
-        [true, "Successfully logged in"]
+        [true, 'Successfully logged in']
       end
     end
 
     def welcome
-      if @coookies.nil?
-        return nil
-      end
+      return nil if @cookies.nil?
 
-      curl = Curl::Easy.new("#{BASE_URL}/authentication.do?value(actions)=welcome")
-      curl.headers["User-Agent"] = USER_AGENT
-      curl.cookies = @cookies.join(";")
+      url = "#{BASE_URL}/authentication.do?value(actions)=welcome"
+      curl = Curl::Easy.new(url)
+      curl.headers['User-Agent'] = USER_AGENT
+      curl.cookies = @cookies.join(';')
       curl.http_get
       curl.body_str[/[^>]*(?<=\, Selamat Datang Di Internet Banking BCA)/]
     end
 
     def balance_inquiry
-      if @coookies.nil?
-        return nil
-      end
+      return nil if @cookies.nil?
 
       curl = Curl::Easy.new("#{BASE_URL}/balanceinquiry.do")
-      curl.headers["User-Agent"] = USER_AGENT
-      curl.cookies = @cookies.join(";")
+      curl.headers['User-Agent'] = USER_AGENT
+      curl.cookies = @cookies.join(';')
       curl.http_post
       html = Nokogiri::HTML(curl.body_str)
       lines = []
-      html.xpath("//table/tr").each do |tr|
-        lines << tr.text.gsub(/[\n]+/, "").strip.gsub(",", ".").gsub(/[ ]{2,}/, ",")
+      html.xpath('//table/tr').each do |tr|
+        lines << tr.text.gsub(/[\n]+/, '').strip.tr(',', '.')
+                   .gsub(/[ ]{2,}/, ',')
       end
       lines.join("\n")
     end
 
     def statement_download
-      if @coookies.nil?
-        return nil
-      end
+      return nil if @cookies.nil?
 
-      curl = Curl::Easy.new("#{BASE_URL}/stmtdownload.do?value(actions)=account_statement")
-      curl.headers["User-Agent"] = USER_AGENT
-      curl.cookies = @cookies.join(";")
+      url = "#{BASE_URL}/stmtdownload.do?value(actions)=account_statement"
+      curl = Curl::Easy.new(url)
+      curl.headers['User-Agent'] = USER_AGENT
+      curl.cookies = @cookies.join(';')
       curl.http_post(
-        Curl::PostField.content("value(r1)", "2"),
-        Curl::PostField.content("value(x)", "1")
+        Curl::PostField.content('value(r1)', '2'),
+        Curl::PostField.content('value(x)', '1')
       )
       curl.body_str
     end
 
     def logout
-      if @coookies.nil?
-        return nil
-      end
+      return nil if @cookies.nil?
 
-      curl = Curl::Easy.new("#{BASE_URL}/authentication.do?value(actions)=logout")
-      curl.headers["User-Agent"] = USER_AGENT
-      curl.cookies = @cookies.join(";")
+      url = "#{BASE_URL}/authentication.do?value(actions)=logout"
+      curl = Curl::Easy.new(url)
+      curl.headers['User-Agent'] = USER_AGENT
+      curl.cookies = @cookies.join(';')
       curl.http_get
       @cookies = nil
       # p curl.body_str
--- test/BCA_test.rb
+++ test/BCA_test.rb
@@ -1,11 +1,20 @@
 require 'test_helper'
 
 class BCATest < Minitest::Test
+  def setup
+    @bca = BCA::Client.new ENV['BCA_USER_ID'], ENV['BCA_PASSWORD']
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::BCA::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_it_fails_when_logging_in
+    result, = @bca.login
+    assert !result
+  end
+
+  def teardown
+    @bca.logout
   end
 end
